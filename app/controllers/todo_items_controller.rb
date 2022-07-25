967a31e5a5ba3f4eed77e1c2f3bb38070e95f994
ndnghi <ndnghi@tma.com.vn>
2022-07-25 11:52:20 +0700

    added ability to mark todo items as complete, and added a bit of styling to show the complete status

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -16,6 +17,11 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
+  def complete
+    @todo_item.update_attribute(:complete_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item complete"
+  end
+
   private
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
@@ -23,5 +29,9 @@ class TodoItemsController < ApplicationController
 
   def todo_item_params
     params[:todo_item].permit(:content)
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
   end
 end
